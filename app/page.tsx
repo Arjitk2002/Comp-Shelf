19793b1229e7c2da222d6f898ae0ed1fe387234b
Arjitk2002 <arjitoffc@gmail.com>
2024-08-29 12:43:30 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,151 @@
-import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { IoLogoWebComponent } from "react-icons/io5";
+import StorageRoundedIcon from "@mui/icons-material/StorageRounded";
+import CodeRoundedIcon from "@mui/icons-material/CodeRounded";
+import ChangeHistoryRoundedIcon from "@mui/icons-material/ChangeHistoryRounded";
+import { auth } from "@clerk/nextjs/server";
 
-export default function Home() {
+function page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="poppins">
+      <Navbar />
+      <CTASection />
+      <Features />
+    </div>
+  );
+}
+
+function Features() {
+  const features = [
+    {
+      id: 1,
+      name: "Centralized Component Library",
+      icon: <StorageRoundedIcon classname="text-sky-500 text-[32px]" />,
+      description: `Organize all your React components in a centralized library. Easily browse, search, and access your saved components whenever you need them`,
+    },
+    {
+      id: 2,
+      name: "Reusable Components",
+      icon: <CodeRoundedIcon classname="text-sky-500 text-[32px]" />,
+      description: `Create and edit your React components directly within our intuitive editor. Write JSX code with syntax highlighter and instant preveiws`,
+    },
+    {
+      id: 3,
+      name: "Version Control & History",
+      icon: <ChangeHistoryRoundedIcon classname="text-sky-500 text-[32px]" />,
+      description: `Create and edit your React components directly within our intuitive editor. Write JSX code with syntax highlighter and instant preveiws`,
+    },
+  ];
+  return (
+    <section className=" py-12 bg-slate-50 mt-12">
+      <div className="mx-auto px-4">
+        <h2 className="text-2xl font-bold text-center">Key Features</h2>
+
+        <div className="mt-12 grid grid-cols-1 md:grid-cols-3 gap-8">
+          {features.map((feature, index) => (
+            <div
+              key={index}
+              className="p-6 bg-white rounded-lg shadow-sm flex flex-col items-center"
+            >
+              <div className="w-20 h-20 rounded-full items-center justify-center flex bg-sky-100">
+                {feature.icon}
+              </div>
+              <h3 className="text-lg font-semibold text-sky-500 text-center">
+                {feature.name}
+              </h3>
+              <p className="text-slate-600 text-[13px] mt-2 text-center w-[80%]">
+                {feature.description}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+function CTASection() {
+  return (
+    <div className="flex flex-col mx-16 items-center mt-[120px] gap-6">
+      <h2 className="font-bold text-2xl text-center">
+        Manage & Create Your React Components
+        <span className={`text-sky-500`}>Effortlessly</span>
+      </h2>
+
+      <p className="text-center text-[15px] w-[510px] max-sm:w-full text-slate-500">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+        veritatis eius inventore, quia incidunt quasi magnam temporibus in
+        excepturi? Eum?
+      </p>
+      <button
+        className={`bg-sky-500 rounded-md px-9 py-3 text-sm font-medium text-white hover:bg-sky-600`}
+        type="button"
+      >{`Let's get started:`}</button>
+    </div>
+  );
+}
+function Navbar() {
+  return (
+    <nav className="flex m-5 max-sm:mt-9 mx-8 items-center justify-between max-sm:flex-col">
+      <Logo />
+      <Buttons />
+    </nav>
+  );
+}
+
+function Logo() {
+  return (
+    <div className="flex gap-2 items-center">
+      {/* // Icon COntainer  */}
+      <div className="bg-sky-500 flex items-center justify-center p-[6px] rounded-md">
+        {/* Icon  */}
+        <div className="w-[26px] h-[26px] items-center justify-center flex">
+          <IoLogoWebComponent className="text-white text-[22px]" />
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      {/* App Name  */}
+      <div className="flex gap-1 text-[22px]">
+        <span className={`font-bold text-sky-500`}>Comp</span>
+        <span className="text-slate-600">Shelf</span>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
+
+function Buttons() {
+  const { userId } = auth();
+  return (
+    <div className="flex gap-2 max-sm:flex-col max-sm:w-full max-sm:mt-8">
+      {!userId ? (
+        <>
+          <Link href="/sign-in">
+            <button
+              className={`max-sm:w-full text-sm border border-sky-500 text-white bg-sky-500 p-[8px] px-6 rounded-md hover:bg-sky-600`}
+            >
+              Sign In
+            </button>
+          </Link>
+          <Link href="sign-up">
+            <button
+              className={`max-sm:w-full text-sm border border-sky-500 text-white bg-sky-500 p-[8px] px-6 rounded-md hover:bg-sky-600`}
+            >
+              Sign Up
+            </button>
+          </Link>
+        </>
+      ) : (
+        <Link href="/dashboard">
+          <button
+            className={`max-sm:w-full text-sm border border-sky-500 text-white bg-sky-500 p-[8px] px-6 rounded-md hover:bg-sky-600`}
+          >
+            Dashboard
+          </button>
+        </Link>
+      )}
+    </div>
+  );
+}
+
+export default page;
